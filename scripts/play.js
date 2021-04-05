0f3f28e22aae4ad145b5e8f640319fda3d91e257
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-04-05 18:35:34 -0300

    Refactoring the "index.html" - Reorder que <scripts> order; Refactoring the "play.js" - Passing the global constants and variable to the "shiftTurn.js"; and Refactoring the "shiftTurn.js" - Structing the code and sending its function invocation to "play.js".

--- scripts/play.js
+++ scripts/play.js
@@ -1,19 +1,16 @@
 /**
  * 
- * GLOBAL CONSTANTS:
- * 
- */
-const firstPlayer = getRandomPlayer();
-const secondPlayer = (firstPlayer === 1) ? 2 : 1;
-
-console.log(`First player: ${firstPlayer}`);
-console.log(`Second player: ${secondPlayer}`);
-
-/**
- * 
  * DOM - MANIPULATION:
  * 
  */
 
- const generalDivEl = createMap();
- document.getElementById('container').appendChild(generalDivEl);
+const generalDivEl = createMap();
+document.getElementById('container').appendChild(generalDivEl);
+
+/**
+ * 
+ * EVENT LISTENERS:
+ * 
+ */
+
+document.getElementById('container').addEventListener("click", currentPlayer);
--- scripts/shiftTurn.js
+++ scripts/shiftTurn.js
@@ -1,19 +1,35 @@
-/**/ 
-let current;
+/**
+ * GLOBAL CONSTANTS:
+ */
+const BLACK_PLAYER = getRandomPlayer();
+const RED_PLAYER = (BLACK_PLAYER === 1) ? 2 : 1;
 
-function currentPlayer () {
-   if (current === undefined) {
-       current = firstPlayer;
-   }
-   if (current === firstPlayer) {
-        current = secondPlayer; 
-   } else {
-       current = firstPlayer;
-   }
+/** 
+ * GLOBAL VARIABLES:
+ */
+let currentPlayer;
 
-    return current;
-}
+// let currentPlayer;
+
+function getRandomPlayer() {
+
+    return Math.round(Math.random() * 1) + 1;
+
+}; //getRandomPlayer()
+
+function currentPlayer() {
+    if (currentPlayer === undefined) {
+        currentPlayer = BLACK_PLAYER;
+    }
+    if (currentPlayer === BLACK_PLAYER) {
+        currentPlayer = RED_PLAYER;
+    } else {
+        currentPlayer = BLACK_PLAYER;
+    }
+
+    return currentPlayer;
+}; //currentPlayer()
 
 currentPlayer()
 
-document.getElementById('container').addEventListener("click", currentPlayer);
+// document.getElementById('container').addEventListener("click", currentPlayer);
