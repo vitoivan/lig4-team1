80dcbfd6e829678f7844268126f8b25b21e8d868
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-04-05 16:03:26 -0300

    Refactoring the "play.js" to "use" the selectFirstPlayer.js".

--- scripts/CreateMap.js
+++ scripts/CreateMap.js
@@ -1,28 +1,37 @@
 const map = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0]
+    [, , , , , ,],
+    [, , , , , ,],
+    [, , , , , ,],
+    [, , , , , ,],
+    [, , , , , ,],
+    [, , , , , ,],
 ];
 
 const createMap = () => {
+
     const generalDiv = document.createElement('div');
-for(let i = 0; i < map.length; i++){
-    const row = map[i];
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('displayFlex');
-    rowDiv.classList.add('row');
-    for (let j = 0; j < row.length; j++) {
-        const div = document.createElement('div');
-        div.id = `block-${i}-${j}`;
-        div.classList.add('block');
-        rowDiv.appendChild(div);
+
+    for (let i = 0; i < map.length; i++) {
+
+        const row = map[i];
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('displayFlex');
+        rowDiv.classList.add('row');
+
+        for (let j = 0; j < row.length; j++) {
+
+            const div = document.createElement('div');
+            div.id = `block-${i}-${j}`;
+            div.classList.add('block');
+            rowDiv.appendChild(div);
+
+        }
+
+        generalDiv.appendChild(rowDiv);
+
     }
-    generalDiv.appendChild(rowDiv);
-}
+
     return generalDiv;
 }
-const generalDiv = createMap();
-document.getElementById('container').appendChild(generalDiv);
+// const generalDiv = createMap();
+// document.getElementById('container').appendChild(generalDiv);
