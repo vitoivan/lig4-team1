6a02b545a140a41dad112a397effad1f866b1b9d
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-04-05 16:47:06 -0300

    Creating and styling the "Map header".

--- scripts/CreateMap.js
+++ scripts/CreateMap.js
@@ -1,37 +1,49 @@
 const map = [
-    [, , , , , ,],
-    [, , , , , ,],
-    [, , , , , ,],
-    [, , , , , ,],
-    [, , , , , ,],
-    [, , , , , ,],
+    [3, 3, 3, 3, 3, 3, 3],
+    [, , , , , , , ],
+    [, , , , , , , ],
+    [, , , , , , , ],
+    [, , , , , , , ],
+    [, , , , , , , ],
+    [, , , , , , , ],
 ];
 
 const createMap = () => {
 
-    const generalDiv = document.createElement('div');
+        const generalDiv = document.createElement('div');
 
-    for (let i = 0; i < map.length; i++) {
+        for (let i = 0; i < map.length; i++) {
 
-        const row = map[i];
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('displayFlex');
-        rowDiv.classList.add('row');
+            const row = map[i];
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('displayFlex');
 
-        for (let j = 0; j < row.length; j++) {
+            if (i === 0)
+                rowDiv.classList.add('tableHeader');
+            else
+                rowDiv.classList.add('tableRow');
 
-            const div = document.createElement('div');
-            div.id = `block-${i}-${j}`;
-            div.classList.add('block');
-            rowDiv.appendChild(div);
+            for (let j = 0; j < row.length; j++) {
+
+                const div = document.createElement('div');
+
+                if (i !== 0) {
+                    div.classList.add('block');
+                    div.id = `block-${i}-${j}`;
+                } else {
+                    div.id = `header-${i}-${j}`;
+                    div.classList.add('header');
+                }
+
+                rowDiv.appendChild(div);
+
+            }
+
+            generalDiv.appendChild(rowDiv);
 
         }
 
-        generalDiv.appendChild(rowDiv);
-
+        return generalDiv;
     }
-
-    return generalDiv;
-}
-// const generalDiv = createMap();
-// document.getElementById('container').appendChild(generalDiv);
+    // const generalDiv = createMap();
+    // document.getElementById('container').appendChild(generalDiv);
