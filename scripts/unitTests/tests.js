108569975614ad402255ac3fd6e1b071f8918a76
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-04-06 10:15:41 -0300

    Implementing the "testAlternatePlayersTurn()."

--- scripts/unitTests/tests.js
+++ scripts/unitTests/tests.js
@@ -1,6 +1,10 @@
 console.log("===\nUNIT TESTS:\n===");
 let hasErros = false;
 
+/**
+ * testAlternatePlayersTurn()
+ */
+
 const testPlayerSelection = () => {
     if (BLACK_PLAYER === 1)
         return RED_PLAYER === 2;
@@ -15,6 +19,27 @@ const resultPlayerSelection = testPlayerSelection();
 const expectedPlayerSelection = true;
 
 console.assert(resultPlayerSelection, expectedPlayerSelection, `The "${testPlayerSelection.name}" is returning ${resultPlayerSelection}, but was expecting ${expectedPlayerSelection}.`);
+
+/**
+ * testAlternatePlayersTurn()
+ */
+const blocksListEl = document.getElementsByClassName('block');
+
+for (let i = 0; i < blocksListEl.length; i++) {
+
+    const blockEl = blocksListEl[i];
+
+    blockEl.addEventListener('click', testAlternatePlayersTurn);
+}
+const testAlternatePlayersTurn = () => {
+
+    hasErros = true;
+}; // testAlternatePlayersTurn()
+
+const resultAlternatePlayersTurn = testAlternatePlayersTurn();
+const expectedAlternatePlayersTurn = true;
+
+console.assert(resultAlternatePlayersTurn, expectedAlternatePlayersTurn, `The "${testAlternatePlayersTurn.name}" is returning ${resultAlternatePlayersTurn}, but was expecting ${expectedAlternatePlayersTurn}.`);
 
 if (!hasErros)
     console.log("Tudo OK!");
