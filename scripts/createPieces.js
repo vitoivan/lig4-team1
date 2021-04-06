a4004223acd9c62b8c66c04965b6a21bb7583b01
Ioane <johnfaumui@gmail.com>
2021-04-06 08:54:51 -0300

    Add condition to check if clicked block is the last empty block of the current column and allow to place a piece into it

--- scripts/createPieces.js
+++ scripts/createPieces.js
@@ -8,22 +8,40 @@ const createPieces = () => {
         const block = blocksListEl[i];
 
         block.addEventListener('click', function() {
+            // Get block parent element and store into a variable
+            let currentBlockParentElement = block.parentElement
 
-            if (block.childNodes.length < 1) {
-                const piece = document.createElement('div');
+            // Create array of all currentBlockParentElement childs
+            let arrayOfBlocks = currentBlockParentElement.childNodes
 
-                if (currentPlayer === RED_PLAYER) {
-                    piece.classList.add('player-1');
+            // Create array for empty blocks
+            let arrayOfEmptyBlocks = []
+
+            // Iterate over arrayOfBlocks, check if block is empty and push it to array of empty blocks
+            for (let i = 0; i < arrayOfBlocks.length; i++) {
+                if (arrayOfBlocks[i].childNodes.length === 0) {
+                    arrayOfEmptyBlocks.push(arrayOfBlocks[i])
                 }
+            }
+            
+            // Check if clicked block is the last element of arrayOfEmptyBlocks and append piece to it
+            if (block === arrayOfEmptyBlocks[arrayOfEmptyBlocks.length - 1]) {
+                if (block.childNodes.length < 1) {
+                    const piece = document.createElement('div');
     
-                if (currentPlayer === BLACK_PLAYER) {
-                    piece.classList.add('player-2');
+                    if (currentPlayer === RED_PLAYER) {
+                        piece.classList.add('player-1');
+                    }
+        
+                    if (currentPlayer === BLACK_PLAYER) {
+                        piece.classList.add('player-2');
+                    }
+        
+                    piece.classList.add('piece');
+                    block.appendChild(piece);
+                    return piece
                 }
-    
-                piece.classList.add('piece');
-                block.appendChild(piece);
-                return piece
-            } 
+            }
         });
     }
 };
