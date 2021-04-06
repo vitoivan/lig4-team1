4d770d9d70b741f0332d4bc5e79d92aab169142f
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-04-06 09:49:10 -0300

    Implementing the "testPlayerSelection()".

---
+++ scripts/unitTests/tests.js
@@ -0,0 +1,20 @@
+console.log("===\nUNIT TESTS:\n===");
+let hasErros = false;
+
+const testPlayerSelection = () => {
+    if (BLACK_PLAYER === 1)
+        return RED_PLAYER === 2;
+
+    if (BLACK_PLAYER === 2)
+        return RED_PLAYER === 1;
+
+    hasErros = true;
+}; // testPlayerSelection()
+
+const resultPlayerSelection = testPlayerSelection();
+const expectedPlayerSelection = true
+
+console.assert(resultPlayerSelection, expectedPlayerSelection, `The "${testPlayerSelection.name}" is returning ${resultPlayerSelection}, but was expecting ${expectedPlayerSelection}.`);
+
+if (!hasErros)
+    console.log("Tudo OK!");
