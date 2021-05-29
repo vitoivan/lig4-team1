37ae9c89bd5c8c35865489ff8270a366c7804fb5
Victor Ivan <viictor.ivan@gmail.com>
2021-05-29 13:27:35 -0300

    reafactor: checkwin

--- scripts/checkWin.js
+++ scripts/checkWin.js
@@ -1,4 +1,4 @@
-/*
+/*----------------------------------------------------------------------------------
     [(i-3, j-3) (i-3, j-2) (i-3, j-1) (i-3, j) (i-3, j+1) (i-3, j+2) (i-3, j+3)]
     [(i-2, j-3) (i-2, j-2) (i-2, j-1) (i-2, j) (i-2, j+1) (i-2, j+2) (i-2, j+3)]
     [(i-1, j-3) (i-1, j-2) (i-1, j-1) (i-1, j) (i-1, j+1) (i-1, j+2) (i-1, j+3)]
@@ -6,7 +6,7 @@
     [(i+1, j-3) (i+1, j-2) (i+1, j-1) (i+1, j) (i+1, j+1) (i+1, j+2) (i+1, j+3)]
     [(i+2, j-3) (i+2, j-2) (i+2, j-1) (i+2, j) (i+2, j+1) (i+2, j+2) (i+2, j+3)]
     [(i+3, j-3) (i+3, j-2) (i+3, j-1) (i+3, j) (i+3, j+1) (i+3, j+2) (i+3, j+3)]
-*/
+-------------------------------------------------------------------------------------*/
 const ValiablePosition = (Ipos, Jpos) => {
     if (Ipos >= 0 && Jpos >= 0 && Ipos < map.length && Jpos < map[0].length) {
         return true;
@@ -14,86 +14,84 @@ const ValiablePosition = (Ipos, Jpos) => {
     return false;
 }
 
-const getNextPosition = (i, j, k, dir) => {
+const getNextPosition = (row, col, konstant, dir) => {
 
     if (dir === 'diag-left-top') {
-        return [i - k, j - k];
+        return [row - konstant, col - konstant];
     }
     if (dir === 'diag-right-top') {
-        return [i - k, j + k];
+        return [row - konstant, col + konstant];
     }
     if (dir === 'diag-right-bottom') {
-        return [i + k, j + k];
+        return [row + konstant, col + konstant];
     }
     if (dir === 'diag-left-bottom') {
-        return [i + k, j - k];
+        return [row + konstant, col - konstant];
     }
     if (dir === 'vertical-top') {
-        return [i - k, j];
+        return [row - konstant, col];
     }
     if (dir === 'vertical-bottom') {
-        return [i + k, j];
+        return [row + konstant, col];
     }
 }
-const checkOneLine = (i, j, p1, p2, voidblock, dir) => {
-    let k = 0;
+const checkOneLine = (row, col, player1, player2, voidblock, direction) => {
+    let konstant = 0;
     let counter1 = 0;
     let counter2 = 0;
-    let Ipos = undefined;
-    let Jpos = undefined;
+    let rowPos = undefined;
+    let colPos = undefined;
 
     do {
-        [Ipos, Jpos] = getNextPosition(i, j, k, dir);
-        // console.log(ValiablePosition(Ipos,Jpos));
-        if (ValiablePosition(Ipos, Jpos)) {
-            if (map[Ipos][Jpos] === 1) {
+        [rowPos, colPos] = getNextPosition(row, col, konstant, direction);
+        if (ValiablePosition(rowPos, colPos)) {
+            const currentCell = map[rowPos][colPos]
+            if (currentCell === 1) {
                 counter1++;
                 counter2 = 0;
             }
-            if (map[Ipos][Jpos] === 2) {
+            if (currentCell === 2) {
                 counter2++;
                 counter1 = 0;
             }
-            if (map[Ipos][Jpos] === voidblock) {
+            if (currentCell === voidblock) {
                 counter1 = 0;
                 counter2 = 0;
             }
             if (counter1 === 4) {
-                return p1;
+                return player1;
             }
             if (counter2 === 4) {
-                return p2;
+                return player2;
             }
-
+        }else{
+            return false;
         }
-        k++;
-    } while (Ipos >= 0 && Jpos < map[i].length && Jpos >= 0 && Ipos < map.length)
-    return false;
+        konstant++;
+    } while (true)
 }
+const checkDiagonal = (player1, player2, voidblock) => {
 
-const checkDiagonal = (p1, p2, voidblock) => {
-
-    let j = 0;
     const lastPos = map[0].length - 1;
-    for (let i = 0; i < map.length; i++) {
+    for (let row = 0; row < map.length; row++) {
 
         //left Side
-        const ResultTopSideLeft = checkOneLine(i, 0, p1, p2, voidblock, 'diag-right-top');
+        const ResultTopSideLeft = checkOneLine(row, 0, player1, player2, voidblock, 'diag-right-top');
         if (ResultTopSideLeft !== false) {
             return ResultTopSideLeft;
         }
 
-        const ResultBotSideLeft = checkOneLine(i, 0, p1, p2, voidblock, 'diag-right-bottom');
+        const ResultBotSideLeft = checkOneLine(row, 0, player1, player2, voidblock, 'diag-right-bottom');
         if (ResultBotSideLeft !== false) {
             return ResultBotSideLeft;
         }
         //Right Side
-        const ResultTopSideRight = checkOneLine(i, lastPos, p1, p2, voidblock, 'diag-left-top');
+        const ResultTopSideRight = checkOneLine(row, lastPos, player1, player2, voidblock, 'diag-left-top');
         if (ResultTopSideRight !== false) {
             return ResultTopSideRight;
         }
 
-        const ResultBotSideRight = checkOneLine(i, lastPos, p1, p2, voidblock, 'diag-left-bottom');
+        const ResultBotSideRight = checkOneLine(row, lastPos, player1, player2, voidblock, 'diag-left-bottom');
         if (ResultBotSideRight !== false) {
             return ResultBotSideRight;
         }
@@ -102,90 +100,85 @@ const checkDiagonal = (p1, p2, voidblock) => {
     return false;
 }
 
-const checkHorizontal = (p1, p2, voidblock) => {
+const checkHorizontal = (player1, player2, voidblock) => {
     let acumulator1 = 0;
     let acumulator2 = 0;
-    for (let i = 0; i < map.length; i++) {
-        const row = map[i];
-        for (let j = 0; j < row.length; j++) {
+    for (let row = 0; row < map.length; row++) {
+        const currentRow = map[row];
+        for (let col = 0; col < currentRow.length; col++) {
+            const currentCell = map[row][col]
 
-            if (map[i][j] === 1) {
+            if (currentCell === 1) {
                 acumulator1++;
                 acumulator2 = 0;
             }
-            if (map[i][j] === 2) {
+            if (currentCell === 2) {
                 acumulator2++;
                 acumulator1 = 0;
             }
-            if (map[i][j] === voidblock) {
+            if (currentCell === voidblock) {
                 acumulator1 = 0;
                 acumulator2 = 0;
             }
 
             if (acumulator1 === 4) {
-                return p1;
+                return player1;
             }
             if (acumulator2 === 4) {
-                return p2;
+                return player2;
             }
         }
         acumulator1 = 0;
         acumulator2 = 0;
     }
+
     return false;
 }
+
 const isDraw = () => {
 
-    for (let i = 0; i < map.length; i++) {
-        const row = map[i]
-        for (let j = 0; j < row.length; j++) {
-            if (map[i][j] === 0) {
+    for (let row = 0; row < map.length; row++) {
+        const currentRow = map[row]
+        for (let col = 0; col < currentRow.length; col++) {
+            if (map[row][col] === 0) {
                 return false;
             }
         }
     }
     return DRAW;
-    /*
-        
-        [1,2,1,2,1,2,1],
-        [1,2,1,2,1,2,1],
-        [2,1,2,1,2,1,2],
-        [2,1,2,1,2,1,2],
-        [1,2,1,2,1,2,1],
-        [1,2,1,2,1,2,1],
-        
-    */
+
 }
-const checkVertical = (j, p1, p2, voidblock) => {
+
+const checkVertical = (col, player1, player2, voidblock) => {
     const lastPos = map.length - 1;
-    const result = checkOneLine(lastPos, j, p1, p2, voidblock, 'vertical-top');
+    const result = checkOneLine(lastPos, col, player1, player2, voidblock, 'vertical-top');
     if (result !== false) {
         return result;
     }
     return false;
 }
-const checkWin = (i, j) => {
-        const voidblock = 0;
-        const horizontal = checkHorizontal(GREEN, PURPLE, voidblock);
-        const vertical = checkVertical(j, GREEN, PURPLE, voidblock);
-        const diagonal = checkDiagonal(GREEN, PURPLE, voidblock);
-        const draw = isDraw(map);
 
-        if (horizontal !== false) {
-            return horizontal;
-        }
-        if (vertical !== false) {
-            return vertical;
-        }
+const checkWin = (row, col) => {
+    const voidblock = 0;
+    const horizontal = checkHorizontal(GREEN, PURPLE, voidblock);
+    const vertical = checkVertical(col, GREEN, PURPLE, voidblock);
+    const diagonal = checkDiagonal(GREEN, PURPLE, voidblock);
+    const draw = isDraw(map);
 
-        if (diagonal !== false) {
+    if (horizontal !== false) {
+        return horizontal;
+    }
+    if (vertical !== false) {
+        return vertical;
+    }
 
-            return diagonal;
-        }
-        if (draw !== false) {
-            return draw;
-        }
+    if (diagonal !== false) {
+        return diagonal;
+    }
+    if (draw !== false) {
+        return draw;
+    }
 
-        return false;
+    return false;
     }
     // Check win retorna ou falso, ou string
